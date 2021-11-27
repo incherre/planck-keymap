8871f5cc1f1f85cbdd012849d4bb6c3701bfb3f4
Banana <banana@bananas-air.lan>
2021-11-27 15:21:02 -0500

    move macros to base layer

--- keymap.c
+++ keymap.c
@@ -89,10 +89,10 @@ const uint32_t PROGMEM unicode_map[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_4x12(
-    KC_TAB,      KC_Q,      KC_W,    KC_E,            KC_R,    KC_T,  KC_Y,  KC_U,   KC_I,           KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,      KC_A,      KC_S,    KC_D,            KC_F,    KC_G,  KC_H,  KC_J,   KC_K,           KC_L,    KC_SCLN, QUOTE,
-    KC_LEFT,     KC_Z,      KC_X,    KC_C,            KC_V,    KC_B,  KC_N,  KC_M,   KC_COMM,        KC_DOT,  KC_SLSH, KC_RGHT,
-    KC__VOLDOWN, KC__VOLUP, KC_LGUI, LALT_T(KC_BSPC), KC_LSFT, LOWER, RAISE, KC_SPC, RCTL_T(KC_DEL), KC_SLEP, KC_BRID, KC_BRIU
+    KC_TAB,  KC_Q,    KC_W,    KC_E,            KC_R,    KC_T,  KC_Y,  KC_U,   KC_I,           KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,            KC_F,    KC_G,  KC_H,  KC_J,   KC_K,           KC_L,    KC_SCLN, QUOTE,
+    KC_LEFT, KC_Z,    KC_X,    KC_C,            KC_V,    KC_B,  KC_N,  KC_M,   KC_COMM,        KC_DOT,  KC_SLSH, KC_RGHT,
+    DM_REC1, DM_PLY1, KC_LGUI, LALT_T(KC_BSPC), KC_LSFT, LOWER, RAISE, KC_SPC, RCTL_T(KC_DEL), DM_REC2, DM_PLY2, DM_RSTP
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12(
@@ -105,8 +105,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12(
     KC_TAB,  KC_GRAVE, KC_LCBR, KC_LPRN,         KC_LBRC, KC_LABK, KC_RABK, KC_RBRC, KC_RPRN,        KC_RCBR, KC_BSLS, KC_BSPC,
     KC_ESC,  KC_1,     KC_2,    KC_3,            KC_4,    KC_5,    KC_6,    KC_7,    KC_8,           NINE,    ZERO,    SIGN,
-    KC_LEFT, KC_HOME,  KC_PGDN, KC_PGUP,         KC_END,  DM_REC1, DM_PLY1, KC_LEFT, KC_DOWN,        KC_UP,   KC_RGHT, KC_RGHT,
-    KC_VOLD, KC_VOLU,  KC_LGUI, LALT_T(KC_BSPC), KC_LSFT, KC_ENT,  XXXXXXX, KC_SPC,  RCTL_T(KC_DEL), DM_RSTP, DM_REC2, DM_PLY2
+    KC_LEFT, KC_HOME,  KC_PGDN, KC_PGUP,         KC_END,  AU_OFF,  AU_ON,   KC_LEFT, KC_DOWN,        KC_UP,   KC_RGHT, KC_RGHT,
+    KC_VOLD, KC_VOLU,  KC_LGUI, LALT_T(KC_BSPC), KC_LSFT, KC_ENT,  XXXXXXX, KC_SPC,  RCTL_T(KC_DEL), KC_BRID, KC_BRIU, KC_SLEP
 )
 
 };
