34aa73408b4a2b56822855bef040ca7b0ab6ba37
Daniel Fitzick <incherre2526@gmail.com>
2022-06-15 17:51:47 -0700

    Fix a couple of trivial details of the rand layout.

--- rules.mk
+++ rules.mk
@@ -1,7 +1,5 @@
 # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes
-# Random layer
-RAND_ENABLE = yes
 
 # Dynamic macros
 DYNAMIC_MACRO_ENABLE = yes
@@ -33,7 +31,3 @@ BLUETOOTH_ENABLE = no
 RGBLIGHT_ENABLE = no
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no
-
-ifeq ($(strip $(RAND_ENABLE)), yes)
-    RAND_ENABLE += -DRAND_ENABLE
-endif
